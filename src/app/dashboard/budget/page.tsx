d4b87d624694d79fcdf8bf736e0570578dc48e36
AbolfazlJM <abolfazljafari563@gmail.com>
2025-07-04 21:28:12 +0330

    feat(dash): pattern of dashboard pages

---
+++ src/app/dashboard/budget/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BudgetPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default BudgetPage;
---
+++ src/app/dashboard/transactions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AddTransactionPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default AddTransactionPage;
