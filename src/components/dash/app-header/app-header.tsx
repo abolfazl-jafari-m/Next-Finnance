5db176ba4318cfbbb64b74c5476e51898e385b36
AbolfazlJM <abolfazljafari563@gmail.com>
2025-07-17 21:01:53 +0330

    feat(dash /app-header) : make header Responsive

--- src/components/dash/app-header/app-header.tsx
+++ src/components/dash/app-header/app-header.tsx
@@ -8,18 +8,18 @@ const ThemeToggle = dynamic(()=>import("@/components/dash/theme-toggle/theme-tog
 function AppHeader() {
     return (
         <header
-            className={"w-full sticky top-0 left-0 right-0 bg-background text-foreground   px-6 py-4.5 shadow-md shadow-neutral-300 dark:shadow-none   dark:bg-sidebar-accent flex items-center justify-between z-50"}>
-            <div className={"flex items-center gap-x-3"}>
-                <SidebarTrigger variant={"outline"} size={"lg"}/>
-                <p className={"font-light"}>خوش امدی ابوالفضل</p>
+            className={"w-full sticky top-0 left-0 right-0 bg-background text-foreground   px-6 max-sm:px-3 py-4.5 shadow-md shadow-neutral-300 dark:shadow-none   dark:bg-sidebar-accent flex items-center justify-between z-50"}>
+            <div className={"flex items-center gap-x-3 max-sm:gap-x-1"}>
+                <SidebarTrigger variant={"outline"} size={"icon"}/>
+                <p className={"font-light text-sm"}>خوش امدی ابوالفضل</p>
                 <ChevronsLeft />
-                <span className={"text-foreground/50"}>{new Date(Date.now()).toLocaleDateString("fa", {
+                <span className={"text-foreground/50 max-sm:text-sm"}>{new Date(Date.now()).toLocaleDateString("fa", {
                     year: "2-digit",
                     month: "long",
                     day: "numeric"
                 })}</span>
             </div>
-            <div className={"flex items-center gap-x-5"}>
+            <div className={"flex items-center gap-x-5 max-sm:gap-x-3"}>
                 <ThemeToggle />
                 <Bell size={18}/>
             </div>
