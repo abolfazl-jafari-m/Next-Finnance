d81e2217c2f5de14014cb1747031a3b7da9053fa
AbolfazlJM <abolfazljafari563@gmail.com>
2025-07-20 20:31:31 +0330

    fix(auth) : optimize background of auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React, {ReactNode} from 'react';
 function Layout({children} : {children : ReactNode}) {
     return (
         <main
-            className={"bg-[url('/images/auth-back.png')] bg-center bg-cover bg-no-repeat w-full h-screen flex items-center justify-center"}>
+            className={"bg-[url('/images/auth-back-min.jpg')] bg-center bg-cover bg-no-repeat w-full h-screen flex items-center justify-center"}>
             {children}
         </main>
     );
