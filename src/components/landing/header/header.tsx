32d8eb165c34ad86f551ac9bb78f56239ca1bd9d
AbolfazlJM <abolfazljafari563@gmail.com>
2025-09-10 10:10:44 +0330

    fix(landing / header) : fix size of heading & btn

--- src/components/landing/header/header.tsx
+++ src/components/landing/header/header.tsx
@@ -8,12 +8,12 @@ import Link from "next/link";
 function Header() {
     return (
         <header>
-            <div className={"flex items-center justify-between px-10 py-5"}>
+            <div className={"flex items-center justify-between px-10 py-5 max-sm:px-5"}>
                 <div className={"flex items-center gap-4"}>
                     <Image src={Logo.src} alt={"logo"} width={36} height={36}/>
-                    <h1 className={"font-bold text-xl"}>دستیار مالی ان وی</h1>
+                    <h1 className={"font-bold text-xl max-sm:text-lg"}>دستیار مالی ان وی</h1>
                 </div>
-                <Button variant={"default"} className={"rounded-2xl px-8"} type={"button"} asChild={true}>
+                <Button variant={"default"} className={"rounded-2xl px-8 max-sm:px-4"} type={"button"} asChild={true}>
                     <Link href={"/login"}>
                         ورود به برنامه
                     </Link>
